c200117925315de82d8402cf9a0fd9e647a37f60
unknown <Keano@Firelink.Firelink>
2021-04-14 12:32:11 -0700

    finished task 3

--- index.js
+++ index.js
@@ -70,10 +70,12 @@ function is31Flavors(array){
    if(array.length === 31){
        return true
    }
-   else(){
+   else {
       return false
    }
 }
+
+is31Flavors(originalFlavors);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -88,10 +90,12 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
-}
+// function addFlavor(array, flavor){
+//    addFlavor.unshift(array, flavor);
+//    return array;
+// }
 
+// addFlavor(originalFlavors, 'rainbow sherbet');
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Houston, we have a problem! There are now 32 flavors in the originalFlavors array! Your task is to remove an item from the end of the array. 
